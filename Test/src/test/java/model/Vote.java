1b502dba116cfdd6b6e1fdc553265c67e7778c86
Andrei-Mihai Canuta <andrei-mihaicanuta@Andrei-Mihais-MacBook-Pro.local>
2022-06-29 14:25:27 +0300

    getter & setter added

--- Test/src/test/java/model/Vote.java
+++ Test/src/test/java/model/Vote.java
@@ -3,4 +3,20 @@ package model;
 public class Vote {
     private String username;
     private boolean vote;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public boolean isVote() {
+        return vote;
+    }
+
+    public void setVote(boolean vote) {
+        this.vote = vote;
+    }
 }
