59d9db06d8fcf5ec56e8bccbbbe3826263db341a
andrei-mihaicanuta <andrei.canuta@metro.digital>
2022-07-06 01:13:13 +0300

    register feature added

--- Test/src/main/java/VotingApplication.java
+++ Test/src/main/java/VotingApplication.java
@@ -1,21 +1,9 @@
 import controller.UserController;
-import dto.UserDto;
-import model.User;
+import requests.RegisterRequest;
 
 public class VotingApplication {
 
     public static void main(String[] args) {
-        UserController controller = new UserController();
-        UserDto u1 = controller.createUser("andi");
-        UserDto u2 = controller.createUser("cezar");
-        controller.createRoom(u1, "room1");
-        controller.createRoom(u1, "room2");
-        controller.joinRoom(u2, "room1_1");
-        controller.vote(u2, "room1_1", 5);
-        controller.vote(u1, "room1_1", 3);
-        controller.vote(u1, "room2_1", 8);
-        System.out.println(u1.toString());
-        System.out.println(u2.toString());
     }
 
 }
